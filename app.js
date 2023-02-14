ddcba9edb02fd11905c7c42fe088b5caffa01ee8
MuhammadAtherSiddiqui <122442314+MuhammadAtherSiddiqui@users.noreply.github.com>
2023-02-14 03:42:13 -0800

    add chapter 14-16

---
+++ app.js
@@ -0,0 +1,106 @@
+//---------CHAPTER NO # 14
+
+//Q NO # 01
+
+//var stdName = []
+
+//Q NO # 02
+
+// var stdName = new Array ();
+// console.log(std);
+
+//Q NO # 03
+
+// var stdName = ["ATHER", "SIDDIQUI","AHMED"];
+// console.log(stdName);
+
+//Q NO # 04
+
+// var num = [ 1, 2, 3, 4, 5];
+// console.log(num);
+
+//Q NO # 05
+
+// var boolean = [ true, false, false, true];
+// console.log(boolean);
+
+//Q NO # 06
+
+// var array = ["SIDDIQUI","25","TRUE"];
+// console.log(array);
+
+//Q NO # 07
+
+// var edu = ["SSC","HSC","BCS","BS","BCOM","MS","M.PHILL.","PHD"];
+// document.write("<h1>Qualifications :</h1>");
+// document.write("<ol><li> "+edu[0]+" </li><li> "+edu[1]+" </li><li> "+edu[2]+" </li><li> "+edu[3]+" </li><li> "+edu[4]+" </li><li> "+edu[5]+" </li><li> "+edu[6]+" </li><li> "+edu[7]+" </li></ol>");
+
+//Q NO # 08
+
+// var std=["ATEHR","OSAMA","ASAD"];
+// var num=["320","230","480"];
+// var tolt= 500;
+
+// document.write("Score of " +std[0]+ " is "+num[0]+". Percentaage: "+(num[0]/tolt*100)+"%");
+// document.write("<br />Score of " +std[1]+ " is "+num[1]+". Percentaage: "+(num[1]/tolt*100)+"%");
+// document.write("<br />Score of " +std[2]+ " is "+num[2]+". Percentaage: "+(num[2]/tolt*100)+"%");
+
+//Q NO # 09
+
+var array=["red","green","orange"];
+//document.write(array);
+
+//Q NO # 09(a)
+
+// var color = window.prompt("What color you want to add at start.");
+// array.unshift(color);
+// document.write(array);
+
+//Q NO # 09(b)
+
+// var color = window.prompt("What color you want to add at end.");
+// array.push(color);
+// //document.write(array);
+
+//Q NO # 09(c)
+
+// array.unshift("black","white");
+// document.write(array);
+
+//Q NO # 09(d)
+
+// array.shift();
+// document.write(array);
+
+//Q NO # 09(e)
+
+// array.pop();
+// document.write(array);
+
+//Q NO # 09(f)
+
+// var index=window.prompt("Which index you want to add a color.");
+// var color = window.prompt("What color you want to add.");
+// array.splice(index, 0, color);
+// document.write(array);
+
+//Q NO # 09(g)
+
+// var index=window.prompt("Which index you want to delete a color.");
+// var color = window.prompt("What color you want to delete.");
+// array.splice(index,color);
+// document.write(array);
+
+//Q NO # 10
+
+// var scores = [34, 56, 21, 67, 92, 55, 44, 33, 77, 90];
+
+// document.write("Scores of studests: "+scores);
+
+// scores.sort(function(a, b) {return a - b;});
+
+// document.write("<br /><br />Ordered scores of studests: "+scores);
+
+
+
+
